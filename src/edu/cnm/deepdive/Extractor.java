c76d46ba89f16ddbe4719c916eaee3d59e78134a
Nick Bennett <nick@nickbenn.com>
2019-08-22 10:07:22 -0600

    Expression lambda referred to by local variable.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -7,9 +7,8 @@ import java.util.TreeSet;
 public class Extractor {
 
   public static void main(String[] args) {
-    Comparator<Character> comparator = (c1, c2) -> {
-      return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
-    };
+    Comparator<Character> comparator = (c1, c2) ->
+        Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
     String source = (args.length > 0) ? args[0] : "Hello, World!";
     Set<Character> chars = new TreeSet<>(comparator);
     for (char c : source.toCharArray()) {
