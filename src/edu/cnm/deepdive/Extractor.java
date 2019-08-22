fb4dc2e3bf01b8097acc9b7a710f6adcc25cc3aa
Nick Bennett <nick@nickbenn.com>
2019-08-22 10:04:50 -0600

    Lambda referred to by local variable.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -7,13 +7,11 @@ import java.util.TreeSet;
 public class Extractor {
 
   public static void main(String[] args) {
+    Comparator<Character> comparator = (c1, c2) -> {
+      return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
+    };
     String source = (args.length > 0) ? args[0] : "Hello, World!";
-    Set<Character> chars = new TreeSet<>(new Comparator<Character>() {
-      @Override
-      public int compare(Character c1, Character c2) {
-        return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
-      }
-    });
+    Set<Character> chars = new TreeSet<>(comparator);
     for (char c : source.toCharArray()) {
       chars.add(c);
     }
