b8123ad5e3fd97421846dd5497ee12ea0bca828e
Nick Bennett <nick@nickbenn.com>
2019-08-22 09:59:38 -0600

    Moved anonymous class to TreeSet constructor invocation.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -7,14 +7,13 @@ import java.util.TreeSet;
 public class Extractor {
 
   public static void main(String[] args) {
-    Comparator<Character> comparator = new Comparator<Character>() {
+    String source = (args.length > 0) ? args[0] : "Hello, World!";
+    Set<Character> chars = new TreeSet<>(new Comparator<Character>() {
       @Override
       public int compare(Character c1, Character c2) {
         return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
       }
-    };
-    String source = (args.length > 0) ? args[0] : "Hello, World!";
-    Set<Character> chars = new TreeSet<>(comparator);
+    });
     for (char c : source.toCharArray()) {
       chars.add(c);
     }
