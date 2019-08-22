e2349017c83fb50561c1370b71b88d7ad9d8c190
Nick Bennett <nick@nickbenn.com>
2019-08-22 09:46:37 -0600

    Moved comparator to pure inner class.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -8,14 +8,14 @@ public class Extractor {
 
   public static void main(String[] args) {
     String source = (args.length > 0) ? args[0] : "Hello, World!";
-    Set<Character> chars = new TreeSet<>(new CaseInsensitiveComparator());
+    Set<Character> chars = new TreeSet<>(new Extractor().new CaseInsensitiveComparator());
     for (char c : source.toCharArray()) {
       chars.add(c);
     }
     System.out.println(chars);
   }
 
-  private static class CaseInsensitiveComparator implements Comparator<Character> {
+  private class CaseInsensitiveComparator implements Comparator<Character> {
 
     @Override
     public int compare(Character c1, Character c2) {
