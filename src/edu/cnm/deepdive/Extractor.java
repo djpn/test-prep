eea1f899f048702c601861af94c07d4eab16af54
Nick Bennett <nick@nickbenn.com>
2019-08-22 09:40:41 -0600

    Moved comparator to static nested class.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -15,13 +15,13 @@ public class Extractor {
     System.out.println(chars);
   }
 
-}
+  private static class CaseInsensitiveComparator implements Comparator<Character> {
 
-class CaseInsensitiveComparator implements Comparator<Character> {
+    @Override
+    public int compare(Character c1, Character c2) {
+      return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
+    }
 
-  @Override
-  public int compare(Character c1, Character c2) {
-    return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
   }
 
 }
