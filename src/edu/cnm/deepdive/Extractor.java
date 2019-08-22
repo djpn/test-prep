136f15a4bfb55fff26889c77e51be43f681ca721
Nick Bennett <nick@nickbenn.com>
2019-08-22 09:52:46 -0600

    Moved comparator to local class.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -7,21 +7,18 @@ import java.util.TreeSet;
 public class Extractor {
 
   public static void main(String[] args) {
+    class CaseInsensitiveComparator implements Comparator<Character> {
+      @Override
+      public int compare(Character c1, Character c2) {
+        return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
+      }
+    }
     String source = (args.length > 0) ? args[0] : "Hello, World!";
-    Set<Character> chars = new TreeSet<>(new Extractor().new CaseInsensitiveComparator());
+    Set<Character> chars = new TreeSet<>(new CaseInsensitiveComparator());
     for (char c : source.toCharArray()) {
       chars.add(c);
     }
     System.out.println(chars);
-  }
-
-  private class CaseInsensitiveComparator implements Comparator<Character> {
-
-    @Override
-    public int compare(Character c1, Character c2) {
-      return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
-    }
-
   }
 
 }
